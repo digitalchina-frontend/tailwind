2dbb08d3860ce39efbadf0a9908c6320e8d75d73
fancn21th <fancn21th@gmail.com>
2022-06-09 11:17:52 +0800

    暂时不启用 PurgeCss

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,10 +9,11 @@ module.exports = {
     plugins: [
       require("tailwindcss"),
       require("autoprefixer"),
-      require("@fullhuman/postcss-purgecss")({
-        content: ["./docs/**/*.vue", "./docs/**/*.md"],
-        defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-      }),
+      // TODO: close by far due to style chaos issue fix the purge issue by refer to https://v2.tailwindcss.com/docs/optimizing-for-production#removing-unused-css
+      // require("@fullhuman/postcss-purgecss")({
+      //   content: [ "./docs/**/*.md"],
+      //   defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      // }),
     ],
   },
 };
